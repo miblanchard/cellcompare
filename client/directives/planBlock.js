f0ff89edda060ef98809ff402937207df1df7c09
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-08-04 18:07:12 -0700

    linted scraper.js corrected Carrier Name to show fullname and moved below plan title in planBlock directive

--- client/directives/planBlock.js
+++ client/directives/planBlock.js
@@ -7,11 +7,12 @@ function planBlock() {
   return {
     restrict: 'E',
     template: `<div class="planContainer">
-                <div class="name">{{plan.carrier}}</div>
                 <div class="name">{{plan.name}}</div>
+                <div class="name">{{plan.carrier}}</div>
                 <hr/>
-                <p>{{plan.dataPlan}}</p><p>{{plan.price}}</p></br>
-                <div class="hidden"><p>{{plan.priceNum}}</p><p>{{plan.dataSize}}</p><p>{{plan.carrier}}</p><p>{{plan.contractPlan}}</p></div>
-              </div>`
+                <p>{{plan.dataPlan}}</p><p>{{plan.price}}</p>
+                <div class="hidden"><p>{{plan.priceNum}}</p><p>{{plan.dataSize}}</p>
+                <p>{{plan.carrier}}</p><p>{{plan.contractPlan}}</p></div>
+              </div>`,
   };
 }
--- server/scraper.js
+++ server/scraper.js
@@ -9,29 +9,29 @@ const webScrapeController = {
   data: [],
   // route to request the AT&T plan data
   getATTData: (req, res, next) => {
-    let attUrl = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=500&data=200&supplier=ATT&network=2';
-    webScrapeController.getCarrierData(req, res, attUrl, next, 'att').then((data) => {
+    const attUrl = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=500&data=200&supplier=ATT&network=2';
+    webScrapeController.getCarrierData(req, res, attUrl, next, 'AT&T').then((data) => {
       next();
     });
   },
   // route to request the Verizon plan data
   getVZWData: (req, res, next) => {
-    let vzwUrl = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=500&data=200&supplier=Verizon-Wireless&network=8';
-    webScrapeController.getCarrierData(req, res, vzwUrl, next, 'vzw').then((data) => {
+    const vzwUrl = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=500&data=200&supplier=Verizon-Wireless&network=8';
+    webScrapeController.getCarrierData(req, res, vzwUrl, next, 'Verizon').then((data) => {
       next();
     });
   },
   // route to request the T-mobile plan data
   getTMOBILEData: (req, res, next) => {
-    let tmoURL = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=-1&data=200&supplier=T-Mobile&network=6';
-    webScrapeController.getCarrierData(req, res, tmoURL, next, 'tmo').then((data) => {
+    const tmoURL = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=-1&data=200&supplier=T-Mobile&network=6';
+    webScrapeController.getCarrierData(req, res, tmoURL, next, 'T-Mobile').then((data) => {
       next();
     });
   },
   // route to request the Sprint plan data
   getSprintData: (req, res, next) => {
-    let sprintURL = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=-1&data=200&supplier=Sprint&network=4';
-    webScrapeController.getCarrierData(req, res, sprintURL, next, 'sprint').then((data) => {
+    const sprintURL = 'https://www.whistleout.com/Ajax/MobilePhones/SearchResults/SingleLineSearch?minutes=-1&sms=-1&data=200&supplier=Sprint&network=4';
+    webScrapeController.getCarrierData(req, res, sprintURL, next, 'Sprint').then((data) => {
       next();
     });
   },
@@ -45,34 +45,50 @@ const webScrapeController = {
    * @return {Promise} webScrapeController.data - Stored each carrier data on a data object
    */
   getCarrierData: (req, res, url, next, carrierName) => {
-    return new Promise (function(resolve, reject) {
+    return new Promise((resolve, reject) => {
       request(url, (error, reqResponse, html) => {
         if (error) {
           console.log(error.message);
         }
-        let $ = cheerio.load(html);
+        const $ = cheerio.load(html);
         $('.results-list')['0'].children
-          .filter((elem) => {
-            return ($(elem).find('h2').html()) ? true : false;
-          })
+          .filter((elem) => ($(elem).find('h2').html()))
           .forEach((elem) => {
             webScrapeController.data.push({
-              name: $(elem).find('h2').html(),
-              dataPlan: $(elem).find('.font-600').eq(2).html(),
-              dataSize: function () {
-                var size = Number($(elem).find('.font-600').eq(2).html().replace(/[A-Za-z]+/g,''));
+              name: $(elem)
+                .find('h2')
+                .html(),
+              dataPlan: $(elem)
+                .find('.font-600')
+                .eq(2)
+                .html(),
+              dataSize: (() => {
+                const size = Number($(elem)
+                  .find('.font-600')
+                  .eq(2)
+                  .html()
+                  .replace(/[A-Za-z]+/g, ''));
                 if (size === 300) {
-                  return .3;
+                  return 0.3;
                 } else if (size === 0) return Infinity;
-                else return size;
-              }(),
-              contractPlan: $(elem).find('.mar-y-3 strong').html(),
-              price: $(elem).find('li.c-gray.font-5.visible-xs').find('strong').html(),
-              priceNum: Number($(elem).find('li.c-gray.font-5.visible-xs').find('strong').html().replace(/[^0-9]+/g,'')),
-              carrier: carrierName
+                return size;
+              })(),
+              contractPlan: $(elem)
+                .find('.mar-y-3 strong')
+                .html(),
+              price: $(elem)
+                .find('li.c-gray.font-5.visible-xs')
+                .find('strong')
+                .html(),
+              priceNum: Number($(elem)
+                .find('li.c-gray.font-5.visible-xs')
+                .find('strong')
+                .html()
+                .replace(/[^0-9]+/g, '')),
+              carrier: carrierName,
             });
           });
-          resolve();
+        resolve();
       });
     });
   },
